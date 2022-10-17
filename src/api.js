4a1b746713aae21232fbf55b3cebd647bf5c323d
Georgi <figoto660@gmail.com>
2022-10-17 13:35:08 -0400

    api add

--- src/api.js
+++ src/api.js
@@ -1,5 +1,3 @@
-const BASE_URL=`https://api.rawg.io/api/games?key=${process.env.REACT_APP_KEY}`
-const popularGames = `&dates=2021-09-30,2022-09-29&ordering=-rating&page_size=10`
-const popularGamesURL = () => `${BASE_URL}${popularGames}`
-
-console.log(popularGamesURL());
+const BASE_URL = `https://api.rawg.io/api/games?key=${process.env.REACT_APP_CATALOG}`;
+const popularGames = `&dates=2021-10-17,2022-10-17&ordering=-rating&page_size=10`;
+export const popularGamesURL = () => `${BASE_URL}${popularGames}`;
